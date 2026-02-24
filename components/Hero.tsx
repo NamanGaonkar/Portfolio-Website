74c624ae5fe7f29fa46966aa0df45ff933b18778
Naman Gaonkar <namanrgaonkar@gmail.com>
2026-02-25 00:13:52 +0530

    fixes

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -28,7 +28,7 @@ export default function Hero() {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6, delay: 0.1 }}
           >
-            hey its me, Naman :)
+            Hey Its Me , Naman :)
           </motion.h1>
           
           <motion.p 
